9142ad838ff1fd6ad7b0289bd06327705a98e543
Anaf Mahbub <axm2604@mavs.uta.edu>
2022-11-01 03:07:58 -0500

    full_credit

---
+++ P08/full_credit/hello_all.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	string Name;
+	cout << "What is your name? ";
+	getline(cin, Name);
+
+	cout << "Hello, " << Name;
+
+
+	return 0;
+}
+
---
+++ P08/full_credit/hello_me.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hello, Anaf Mahbub!";
+
+    return 0;
+}
